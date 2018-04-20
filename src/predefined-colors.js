d498f3edc6645555d86f2475fccb95df335af9ab
Alex Chernov <alexusblack@gmail.com>
2018-04-20 10:35:03 +1000

    making colors to match design

--- src/predefined-colors.js
+++ src/predefined-colors.js
@@ -2,19 +2,19 @@ import Color from 'color';
 
 const predefinedColors = {
   red: { 
-    uiColor: Color.rgb(255, 70, 70),
+    uiColor: Color.rgb(238, 54, 54),
     moduleColor: Color('red')
   },
   green: { 
-    uiColor: Color.rgb(64, 215, 139),
+    uiColor: Color.rgb(1, 207, 88),
     moduleColor: Color('green')
   },
   blue: { 
-    uiColor: Color.rgb(0, 191, 222),
+    uiColor: Color.rgb(17, 169, 255),
     moduleColor: Color('blue')
   },
   yellow: { 
-    uiColor: Color.rgb(251, 236, 0),
+    uiColor: Color.rgb(252, 187, 64),
     moduleColor: Color('yellow')
   },
   white: { 
