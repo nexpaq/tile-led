25aa922ade0183c45c949f1670d2755f6b6c5f1c
Alex Chernov <alexusblack@gmail.com>
2018-04-24 15:36:59 +1000

    create base class for led themes

--- src/themes/CandleFlickerTheme.js
+++ src/themes/CandleFlickerTheme.js
@@ -1,22 +1,16 @@
-export default class CandleFlickerTheme {
-  constructor(commandBufferFilter) {
-    if(commandBufferFilter == null) throw new Exception('Command buffer required for the theme');
+import LedThemeBase from './LedThemeBase';
 
-    this.active = false;
-    this.period = 100; // ms
+export default class CandleFlickerTheme extends LedThemeBase {
+  constructor(commandBufferFilter) {
+    super();
+    if(commandBufferFilter == null) throw new Exception('Command buffer required for the theme');
 
     this._commandBufferFilter = commandBufferFilter;
     this._color = [100, 55, 0];
   }
 
-  start() {
-    if(this.active) return;
-    this.active = true;
-    this._loop();
-  }
-
   stop() {
-    this.active = false;
+    super.stop();
     this._commandBufferFilter.setCommand(() => Moduware.v0.API.Module.SendCommand(Moduware.Arguments.uuid, 'SetRGB', [0,0,0]));
   }
 
@@ -24,19 +18,6 @@ export default class CandleFlickerTheme {
     const fraction = this._getRandomArbitrary(0.7, 1.2);
     const fractionColor = this._color.map(c => c * fraction);
     this._commandBufferFilter.setCommand(() => Moduware.v0.API.Module.SendCommand(Moduware.Arguments.uuid, 'SetRGB', fractionColor));
-  }
-
-  async _loop() {
-    while(this.active) {
-      await this.update();
-      await this._sleep(this.period);
-    }
-  }
-
-  _sleep(milliseconds) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => resolve(), milliseconds);
-    });
   }
 
   _getRandomArbitrary(min, max) {
---
+++ src/themes/LedThemeBase.js
@@ -0,0 +1,31 @@
+export default class LedThemeBase {
+  constructor() {
+    this.active = false;
+    this.period = 100; // ms
+  }
+
+  start() {
+    if(this.active) return;
+    this.active = true;
+    this._loop();
+  }
+
+  stop() {
+    this.active = false;
+  }
+
+  update() { }
+
+  async _loop() {
+    while(this.active) {
+      await this.update();
+      await this._sleep(this.period);
+    }
+  }
+
+  _sleep(milliseconds) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(), milliseconds);
+    });
+  }
+}
