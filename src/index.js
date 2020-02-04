24cc1318d154183e3f337f55c230d87f3b847626
Clint <clintp1@gmail.com>
2020-02-04 16:07:47 +1100

    add webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import 'regenerator-runtime/runtime';
+import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
+import './components/my-app.js';
+
+console.log('hello dude, from index.js');
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "./index.html"
+    }),
+  ]
+}
