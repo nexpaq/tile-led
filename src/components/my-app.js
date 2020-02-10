38e8c3807886d6ba786b5b4490bea75d5ca9c9a9
moemen.hegazy@gmail.com <moemen.hegazy@gmail.com>
2020-02-10 22:48:41 +0200

    adding styles from tile template

--- src/components/my-app.js
+++ src/components/my-app.js
@@ -60,91 +60,148 @@ class MyApp extends connect(store)(LitElement) {
 			SwitchControl,
 			ThemeButton,
 			Wheel,
-			Variables
-			// css`
-      //   :host {
-      //     overflow-y: scroll;
-      //     position: absolute;
-      //     width: 100%;
-      //     height: 100%;
-      //     box-sizing: border-box;
-      //   }
+			Variables,
+			css`
+        :host {
+          display: block;
 
-      //   :host {
-      //     padding-top: 55px;
-      //     font-family: Roboto;
-      //     /*height: calc(100vh - 55px);*/
-      //   }
-				
-      //   :host([platform='ios']) {
-      //     padding-top: 44px;
-			// 		/*height: calc(100vh - 44px);*/
-			// 		font-family: -apple-system, 'SF UI Text', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-      //   }
+          --app-drawer-width: 256px;
 
-      //   :host([platform='android']) moduware-header {
-      //     box-shadow: none;
-      //   }
+          --app-primary-color: #E91E63;
+          --app-secondary-color: #293237;
+          --app-dark-text-color: var(--app-secondary-color);
+          --app-light-text-color: white;
+          --app-section-even-color: #f7f7f7;
+          --app-section-odd-color: white;
 
-      //   moduware-header {
-      //     position: absolute;
-      //   }
+          --app-header-background-color: white;
+          --app-header-text-color: var(--app-dark-text-color);
+          --app-header-selected-color: var(--app-primary-color);
 
+          --app-drawer-background-color: var(--app-secondary-color);
+          --app-drawer-text-color: var(--app-light-text-color);
+          --app-drawer-selected-color: #78909C;
+        }
 
-      //   :host {
-      //     --moduware-brand-color: #d02e3d;
-      //     --item-container-z-index: 0; /* css variable for list-item .container z-index */
+        app-header {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          text-align: center;
+          background-color: var(--app-header-background-color);
+          color: var(--app-header-text-color);
+          border-bottom: 1px solid #eee;
+        }
 
-      //     display: flex;
-      //     flex-direction: column;
-      //     justify-content: stretch;
-      //   }
+        .toolbar-top {
+          background-color: var(--app-header-background-color);
+        }
 
+        [main-title] {
+          font-family: 'Pacifico';
+          text-transform: lowercase;
+          font-size: 30px;
+          /* In the narrow layout, the toolbar is offset by the width of the
+          drawer button, and the text looks not centered. Add a padding to
+          match that button */
+          padding-right: 44px;
+        }
 
-      //   [main-title] {
-      //     font-family: 'Pacifico';
-      //     text-transform: lowercase;
-      //     font-size: 30px;
-      //     /* In the narrow layout, the toolbar is offset by the width of the
-      //     drawer button, and the text looks not centered. Add a padding to
-      //     match that button */
-      //     padding-right: 44px;
-      //   }
+        .toolbar-list {
+          display: none;
+        }
 
-      //   /* Workaround for IE11 displaying <main> as inline */
-      //   main {
-      //     display: flex;
-      //   }
+        .toolbar-list > a {
+          display: inline-block;
+          color: var(--app-header-text-color);
+          text-decoration: none;
+          line-height: 30px;
+          padding: 4px 24px;
+        }
 
-      //   .main-content {
-      //     height: 100%;
-      //     flex-grow: 1;
-      //     /* box-sizing: border-box;
-      //     -moz-box-sizing: border-box;
-      //     -webkit-box-sizing: border-box; */
-      //     /*margin-top: 72px;*/
-      //   }
+        .toolbar-list > a[selected] {
+          color: var(--app-header-selected-color);
+          border-bottom: 4px solid var(--app-header-selected-color);
+        }
 
-      //   :host([platform='ios']) .main-content {
-			// 		background-color: #F9F9F9 !important;
-      //   }
-        
-      //   :host([platform='android']) .main-content {
-			// 		background-color: #F2F2F2 !important;
-			// 	}
+        .menu-btn {
+          background: none;
+          border: none;
+          fill: var(--app-header-text-color);
+          cursor: pointer;
+          height: 44px;
+          width: 44px;
+        }
 
-      // /*.page {
-      //     display: none;
-      //   }*/
-      //   .page {
-      //     background-color: white;
-      //   }
+        .drawer-list {
+          box-sizing: border-box;
+          width: 100%;
+          height: 100%;
+          padding: 24px;
+          background: var(--app-drawer-background-color);
+          position: relative;
+        }
 
-      //   .page[active] {
-      //     display: flex;
-      //   }
+        .drawer-list > a {
+          display: block;
+          text-decoration: none;
+          color: var(--app-drawer-text-color);
+          line-height: 40px;
+          padding: 0 24px;
+        }
 
-      // `
+        .drawer-list > a[selected] {
+          color: var(--app-drawer-selected-color);
+        }
+
+        /* Workaround for IE11 displaying <main> as inline */
+        main {
+          display: block;
+        }
+
+        .main-content {
+          padding-top: 64px;
+          min-height: 100vh;
+        }
+
+        .page {
+          display: none;
+        }
+
+        .page[active] {
+          display: block;
+        }
+
+        footer {
+          padding: 24px;
+          background: var(--app-drawer-background-color);
+          color: var(--app-drawer-text-color);
+          text-align: center;
+        }
+
+        /* Wide layout: when the viewport width is bigger than 460px, layout
+        changes to a wide layout */
+        @media (min-width: 460px) {
+          .toolbar-list {
+            display: block;
+          }
+
+          .menu-btn {
+            display: none;
+          }
+
+          .main-content {
+            padding-top: 107px;
+          }
+
+          /* The drawer button isn't shown in the wide layout, so we don't
+          need to offset the title */
+          [main-title] {
+            padding-right: 0px;
+          }
+        }
+      `
 		];
 	}
 
